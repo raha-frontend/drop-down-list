cae9a49fc55a2c3d32d2bbeccbb37dc6c639168a
Rahman <rahmangadzh1994@gmail.com>
2021-01-25 22:04:08 +0300

    Поправил по комментариям

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-var accordeon = document.querySelector('.accordeon');
+document.body.addEventListener('click', function ({ target }) {
+    if (target.classList.contains('accordeon__btn')) {
+        // const list = target.nextElementSibling;
+        // list.classList.toggle('accordeon__list-show');
 
-accordeon.addEventListener('click', function (event) {
-    var element = event.target;
-
-    if (element.classList.contains('accordeon__btn')) {
-        var accordeon__btn = element.nextElementSibling;
-
-        accordeon__btn.classList.toggle('accordeon__list-show');
+        target.classList.toggle('accordeon__btn_active');
     }
 });
